a6862ce365b36b859fb59c97c5ee86d4d2a38042
Aremu Smog <aremuoluwagbamila@gmail.com>
2021-08-19 08:27:05 +0100

    feat: data plot

--- src/pages/country/chart/index.js
+++ src/pages/country/chart/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import {
   select,
-  csv,
   scaleLinear,
   scaleTime,
   extent,
@@ -10,11 +9,14 @@ import {
   line,
   curveBasis,
   curveCardinal,
+  timeParse,
+  utcParse,
   max,
   min,
+  timeYears,
+  timeMonth,
+  timeYear,
 } from "d3";
-
-import * as d3 from "d3";
 
 const CountryChart = ({ data }) => {
   const chartRef = useRef();
@@ -22,10 +24,12 @@ const CountryChart = ({ data }) => {
   useEffect(() => {
     const svg = select(chartRef.current);
 
+    const parser = timeParse("%Y-%m-%dT%H:%M:%S%Z");
     const xValue = (d) => {
       const theDate = new Date(d.Date);
+      //   const theDate = parser(d.Date);
       //   return d.Date;
-
+      console.log(theDate);
       return theDate;
     };
     const xAxisLabel = "Time";
@@ -33,63 +37,62 @@ const CountryChart = ({ data }) => {
     const yAxisLabel = "Temperature";
 
     const margin = { top: 60, right: 40, bottom: 88, left: 105 };
-    const innerWidth = 500;
-    const innerHeight = 900;
+    const innerWidth = 960;
+    const innerHeight = 500;
 
     const xScale = scaleTime()
-      .domain([
-        min(data, (d) => xValue(d.Date)),
-        max(data, (d) => xValue(d.Date)),
-      ])
+      .domain([min(data, (d) => xValue(d)), max(data, (d) => xValue(d))])
       .range([0, innerWidth]);
 
     const yScale = scaleLinear()
-      .domain([
-        min(data, (d) => yValue(d.Active)),
-        max(data, (d) => yValue(d.Active)),
-      ])
+      .domain([min(data, (d) => yValue(d)), max(data, (d) => yValue(d))])
       .range([innerHeight, 0]);
 
     const g = svg
       .append("g")
       .attr("transform", `translate(${margin.left},${margin.top})`);
 
-    const xAxis = axisBottom(xScale).tickSize(-innerHeight).tickPadding(15);
+    const xAxis = axisBottom(xScale);
 
-    const yAxis = axisLeft(yScale).tickSize(-innerWidth).tickPadding(10);
+    // svg
+    //   .append("g")
+    //   .attr("transform", "translate(0,50)")
+    //   .call(xAxis.ticks(timeYear));
+
+    // const yAxis = axisLeft(yScale).tickSize(-innerWidth).tickPadding(10);
 
     const activeLine = line()
       .x((d) => xScale(xValue(d)))
       .y((d) => yScale(yValue(d)))
       .curve(curveCardinal);
 
-    const yAxisG = g.append("g").call(yAxis);
-    yAxisG.selectAll(".domain").remove();
+    // const yAxisG = g.append("g").call(yAxis);
+    // yAxisG.selectAll(".domain").remove();
 
-    yAxisG
-      .append("text")
-      .attr("class", "axis-label")
-      .attr("y", -60)
-      .attr("x", -innerHeight / 2)
-      .attr("fill", "black")
-      .attr("transform", `rotate(-90)`)
-      .attr("text-anchor", "middle")
-      .text(yAxisLabel);
+    // yAxisG
+    //   .append("text")
+    //   .attr("class", "axis-label")
+    //   .attr("y", -60)
+    //   .attr("x", -innerHeight / 2)
+    //   .attr("fill", "black")
+    //   .attr("transform", `rotate(-90)`)
+    //   .attr("text-anchor", "middle")
+    //   .text(yAxisLabel);
 
-    const xAxisG = g
-      .append("g")
-      .call(xAxis)
-      .attr("transform", `translate(0,${innerHeight})`);
+    // const xAxisG = g
+    //   .append("g")
+    //   .call(xAxis)
+    //   .attr("transform", `translate(0,${innerHeight})`);
 
-    xAxisG.select(".domain").remove();
+    // xAxisG.select(".domain").remove();
 
-    xAxisG
-      .append("text")
-      .attr("class", "axis-label")
-      .attr("y", 80)
-      .attr("x", innerWidth / 2)
-      .attr("fill", "black")
-      .text(xAxisLabel);
+    // xAxisG
+    //   .append("text")
+    //   .attr("class", "axis-label")
+    //   .attr("y", 80)
+    //   .attr("x", innerWidth / 2)
+    //   .attr("fill", "black")
+    //   .text(xAxisLabel);
 
     g.append("path")
       //   .attr("fill", "none")
